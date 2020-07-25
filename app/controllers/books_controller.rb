80854eba54213d92de3cf50bd0104ccca98efb06
vagrant <vagrant@localhost.localdomain>
2020-07-25 11:23:35 +0000

    [finish]4-8　アプリケーション基礎

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,46 @@
 class BooksController < ApplicationController
 
-	def books
-		@book = Book.
-		@books = Book.all
+	def new
 	end
 
 	def create
-		book = Book.new(book_params)
-		book.save
-		redirect_to :action => 'show_books'
+		@book = Book.new(book_params)
+		if @book.save
+			redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+		else
+			@books = Book.all
+			render "books/index"
+		end
 	end
 
 	def show
 		@book = Book.find(params[:id])
 	end
 
+	def index
+		@books = Book.all
+		@book = Book.new
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		book = Book.find(params[:id])
+		book.update(book_params)
+		redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+	end
+
+	def destroy
+		book = Book.find(params[:id])
+		if book.destroy
+			redirect_to books_path, notice: 'Book was successfully destroyed.'
+		end
+	end
+
 	private
 	def book_params
 		params.require(:book).permit(:title, :body)
 	end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,16 @@ Rails.application.routes.draw do
 
   get '/' => 'homes#top'
 
-  get 'books' => 'books#books'
+  get 'books' => 'books#index'
 
   post 'books' => 'books#create'
 
-  get 'books/:id' => 'books#show', as: 'show_books'
+  get 'books/:id' => 'books#show', as: 'book'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
+  delete 'books/:id' => 'books#destroy', as:'destroy_book'
 
 end
